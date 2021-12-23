b227824665eec11fcbc6c4dc989820583e7f3cd3
malikibrarnaeem <95858450+malikibrarnaeem@users.noreply.github.com>
2021-12-23 15:43:36 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,81 @@
+var inp1 = document.getElementById("input1") 
+var inp2 = document.getElementById("input2") 
+var inp3 = document.getElementById("input3") 
+var inp4 = document.getElementById("input4") 
+var inp5 = document.getElementById("input5") 
+var inp6 = document.getElementById("input6") 
+var inp7 = document.getElementById("input7") 
+var inp8 = document.getElementById("input8") 
+var inp9 = document.getElementById("input9") 
+var inp10 = document.getElementById("input10") 
+var inp11 = document.getElementById("input11") 
+var inp12 = document.getElementById("input12") 
+var inp13 = document.getElementById("input13") 
+var inp14 = document.getElementById("input14") 
+
+
+
+var selT = document.getElementById("select1")
+var selT1 = document.getElementById("select2")
+// function formInputs(display){
+
+//     var a = inp1.value
+//     var b = inp2.value
+//     var c = inp3.value
+//     var d = inp4.value
+//     var e = inp5.value
+//     var f = inp6.value
+//     var g = inp7.value
+//     var h = inp8.value
+//     var i = inp9.value
+//     var j = inp10.value
+//     var k = inp11.value
+//     var l = inp12.value
+//     var m = inp13.value
+//     var n = inp14.value
+//     var o = selT.value
+//     var p = selT1.value
+   
+   
+
+
+  
+//    }
+
+
+   function showDetails(){
+    var a = inp1.value
+    var ba = document.getElementById("peragraph1").innerHTML = a;
+    var b = inp2.value
+    var ab = document.getElementById("peragraph2").innerHTML = b;
+    var c = inp3.value
+    var ca = document.getElementById("peragraph3").innerHTML = c;
+    var d = inp4.value
+    var da = document.getElementById("peragraph3").innerHTML = d;
+    var e = inp5.value
+    var ea = document.getElementById("peragraph4").innerHTML = e;
+    var f = inp6.value
+    var fa = document.getElementById("peragraph5").innerHTML = f;
+    var g = inp7.value
+    var ga = document.getElementById("peragraph6").innerHTML = g;
+    var h = inp8.value
+    var ha = document.getElementById("peragraph7").innerHTML = h;
+    var i = inp9.value
+    var ia = document.getElementById("peragraph8").innerHTML = i;
+    var j = inp10.value
+    var ja = document.getElementById("peragraph9").innerHTML = j;
+    var k = inp11.value
+    var ka = document.getElementById("peragraph10").innerHTML = k;
+    var l = inp12.value
+    var la = document.getElementById("peragraph11").innerHTML = l;
+    var m = inp13.value
+    var ma = document.getElementById("peragraph12").innerHTML = m;
+    var n = inp14.value
+    var na = document.getElementById("peragraph13").innerHTML = n;
+
+   
+   
+
+
+    
+   }
